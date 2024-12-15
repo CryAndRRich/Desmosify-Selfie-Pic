935da43701c96de6c0916edc9bc3111cc0ac6493
CryAndRRich <willshane2006@gmail.com>
2024-12-15 19:20:01 +0700

    Update histogram_equalization.py

--- filter/histogram_equalization.py
+++ filter/histogram_equalization.py
@@ -1,1 +1,22 @@
+import numpy as np
 
+def histogram_equalization(img):
+    hist = np.zeros((256,), np.uint8)
+    h, w = img.shape[:2]
+    for i in range(h):
+        for j in range(w):
+            hist[img[i][j]] += 1
+
+    hist = hist.ravel()
+
+    cumulator = np.zeros_like(hist, np.float64)
+    for i in range(len(cumulator)):
+        cumulator[i] = hist[:i].sum()
+    new_hist = (cumulator - cumulator.min())/(cumulator.max() - cumulator.min()) * 255
+    new_hist = np.uint8(new_hist)
+
+    for i in range(h):
+        for j in range(w):
+            img[i,j] = new_hist[img[i,j]]
+    
+    return img

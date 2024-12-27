ce578722b2de201272c5b06580c9312e472e6b82
CryAndRRich <willshane2006@gmail.com>
2024-12-27 17:30:22 +0700

    Update __init__.py

--- filter/contrast_enhancement_technique/__init__.py
+++ filter/contrast_enhancement_technique/__init__.py
@@ -1,6 +1,6 @@
-from filter.contrast_enhancement_technique.gamma_correction import gamma_correction
-from filter.contrast_enhancement_technique.histogram_equalization import histogram_equalization
-from filter.contrast_enhancement_technique.luminance_adaption import luminance_adaption
+from .gamma_correction import gamma_correction
+from .histogram_equalization import histogram_equalization
+from .luminance_adaption import luminance_adaption
 
 def contrast_enhancement(image, techniques):
     for technique in techniques:
